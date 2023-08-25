30cdf4cf9ed5688a8442424983b1d4522769a25d
dengidengi <dengianassi@gmail.com>
2023-08-25 16:23:59 +0200

    Before branching

--- auctions/views.py
+++ auctions/views.py
@@ -1,10 +1,20 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
 from .models import User
+
+categories = ["Rock","Pop","Jazz","HipHop/RnB","Electronic/Dance"]
+class createListingForm(forms.Form):
+    title = forms.CharField(attrs={'placeholder':'Title'})
+    description = forms.CharField(widget=forms.TextArea(attrs={'placeholder':'Provide a brief description of the article'}))
+    category = forms.ChoiceField(choices=categories)
+    startingBid = forms.DecimalField(min_value=0, decimal_places=2, max_digits=10)
+    imageUrls = forms.UrlField()
 
 
 def index(request):
@@ -63,10 +73,11 @@ def register(request):
         return render(request, "auctions/register.html")
 
 
+@login_required
 def createListing(request):
-    if not request.user.is_authenticated:
-        return render(request,"auctions/login.html",{
-            "message":"You must login before creating a new listing"
-        })
-    else:
-        return render(request,"auctions/createListings.html")
+    if request.method == "POST":
+        
+   
+    return render(request,"auctions/createListings.html",{
+        "categories":categories
+    })
